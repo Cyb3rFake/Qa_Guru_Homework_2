c812d8cd57e6eb183082960e68e18cce4788d5e2
korvyakov_fn <enemy1987@yandex.ru>
2022-03-31 15:31:22 +0300

    new branch "anotherText"

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("!!@#Conflict_String!1231535t344f");
+        System.out.println("AnotherText");
 	// write your code here
     }
 }
