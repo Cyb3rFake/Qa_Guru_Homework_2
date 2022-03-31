ecca7be62265047b37c6795a99f0beac148d6513
korvyakov_fn <enemy1987@yandex.ru>
2022-03-31 15:23:21 +0300

    text changed

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Conflict_String!");
+        System.out.println("!!@#Conflict_String!1231535t344f");
 	// write your code here
     }
 }
